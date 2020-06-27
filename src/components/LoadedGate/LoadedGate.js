c1e9f9d53d5d5b2510e38fc04bd2c70101c37212
Steven Zhao <steven@zhao.io>
2020-06-27 14:56:18 -0700

    Update loading gate style

--- src/components/LoadedGate/LoadedGate.js
+++ src/components/LoadedGate/LoadedGate.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {Spinner} from '@ui-kitten/components';
+import LinearGradient from 'react-native-linear-gradient';
+import {animate} from '../../style/animation';
 
 const LoadedGate = ({children}) => {
   const initialized = useSelector(state => state.initialized);
 
-  if (!initialized) return <Spinner />;
+  if (!initialized)
+    return (
+      <LinearGradient
+        colors={['#21282F', '#522166']}
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Spinner />
+      </LinearGradient>
+    );
   return <>{children}</>;
 };
 
